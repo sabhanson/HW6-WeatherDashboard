fca492f1583eb563a1a854c8a84c3f6293bc749c
Sabrina Hanson <sabhanson7@gmail.com>
2022-01-30 10:47:18 -0800

    previous search city buttons are working

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,34 @@
+//START OVER PSUEDOCODE
+
+//FIRST, TEXTINPUT FIELD "SEARCH FOR A CITY" ADD EVENT LISTENER CLICK ON SUBMIT BUTTON
+    //THEN
+        //TODAY'S WEATHER EMOJI, TEMP, WIND, AND HUMIDITY APPEAR IN TOP LARGE SECTION
+        //EACH OF THE NEXT FIVE DAYS EMOJI, TEMP, WIND, AND HUMIDITY APPEAR IN THE DAY CARDS
+//PREVIOUS SEARCHED CITY IS BEING MADE INTO A BUTTON AND SAVED ON THE LEFT SIDE
+    //SAVE PREVIOUS CITIES INTO AN ARRAY AND HAVE THE ARRAY PUT INTO BUTTONS??
+
+//API CALL TO RECEIVE TODAY'S EMOJI, TEMP, WIND, HUMIDITY
+//API CALL TO RECEIVE 5 DAY EMOJIS, TEMPS, WINDS, HUMIDITIES
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //submit button click listener runs function to bring up specific city based on user input
 //fetch request API to gather necessary data
     //temperature
     //wind
     //humidity
+    //UV index value
 
 //localStorage list of previously searched locations, when they are clicked, city is reentered into the search bar to repopulate info on the page
     //localstorage will store previously searched city
@@ -49,6 +75,11 @@ var card4 = document.querySelector('.four');
 card4.textContent = moment().add(4, 'day').format('l');
 var card5 = document.querySelector('.five');
 card5.textContent = moment().add(5, 'day').format('l');
+var tempToday = document.querySelector('.todayTemp')
+
+
+
+test();
 
 function test() {
     var searchInput = document.querySelector('.searchBar');
@@ -66,6 +97,8 @@ function test() {
         console.log(data.weather[0].icon + '.png')
         //temperature
         console.log(data.main.temp + ' deg farenheit')
+        var temp = data.main.temp;
+        tempToday.textContent = 'Temp: ' + temp + ' \xB0F'
         //wind
         console.log(data.wind.speed + ' MPH')
         //humidity
@@ -76,16 +109,35 @@ function test() {
     };
 
 
-submitBtn.addEventListener('click', function() {
-    var city = searchInput.value;
-    console.log(city);
-    todayName.textContent = city;
-    localStorage.setItem("prevSearch",JSON.stringify(city))
-    var yeah = localStorage.getItem("prevSearch")
-    placeholdBtn.innerHTML = JSON.parse(yeah)
-    test();
-})
+    
+    submitBtn.addEventListener('click', function() {
+        var saveCity = searchInput.value;
+        todayName.textContent = saveCity;
+        localStorage.setItem("newCity", saveCity);
+        var newBtn = document.createElement("button");
+        newBtn.type = 'button',
+        newBtn.innerHTML = saveCity;
+        newBtn.className = "placeholderBtn"
+        //this is working to append the button to the list of buttons
+        buttonContainer.appendChild(newBtn);
+        localStorage.getItem("newCity")
+    })
+    
 
-//submit button takes the value (city) that was entered into the search bar. needs to populate that info into the container
-//then run api function to pull data into container
-
+    newBtn.addEventListener('click', function () {
+        todayName.textContent = newBtn;
+    })
+    
+    
+    //submit button takes the value (city) that was entered into the search bar. needs to populate that info into the container
+    //then run api function to pull data into container
+    
+    
+    
+    
+    //IDEAS
+    //TODO: for each day to show up in the 5 day forecast cards:
+    //add a data attribute "data-day" to each card. example: data-day: 1 would then be in a function for moment moment().add(data-day, 'day').format('l)
+    
+   
+    
